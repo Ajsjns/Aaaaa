24fad57170c3164b7eee63fad6b643004ecfa559
Luffy <othmaneboughari@gmail.com>
2024-09-30 23:16:35 +0200

    Update فعاليات.js

--- plugins/فعاليات.js
+++ plugins/فعاليات.js
@@ -3,7 +3,7 @@ let handler = async (m, {command, conn, usedPrefix}) => {
 let res = (await axios.get(`https://raw.githubusercontent.com/socona12/TheMystic-Bot-MD/master/src/JSON/Anime-F3alya.json`)).data  
 let haha = await res[Math.floor(res.length * Math.random())]    
 conn.sendFile(m.chat, haha, 'error.jpg', `
-〄━━━━━⌬〔Nano〕⌬━━━━━〄
+〄━━━━━⌬〔obito〕⌬━━━━━〄
 
 *⌬🕹️┋السوأل » احزر الشخصية*
 
